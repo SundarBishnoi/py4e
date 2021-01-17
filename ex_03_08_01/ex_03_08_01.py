77f6af849955463f17ec1688fe9be563772c9ba2
Sundar Bishnoi <sundarlal2929@gmail.com>
2021-01-17 10:56:12 +0530

    Update ex_03_08_01.py
    addition

--- ex_03_08_01/ex_03_08_01.py
+++ ex_03_08_01/ex_03_08_01.py
@@ -1,4 +1,11 @@
 friends = ['serum','reddy','albert']
 print(friends)
 print(friends[1])
+for i in [1,15,46,15,12]:
     print(i)
+print('BlastOff!')
+
+print(len(friends))
+
+print(range(0,4))
+print(range(len(friends)))
