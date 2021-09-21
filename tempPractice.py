81020ec07019bacb5426c614dfc58fe93a480580
Sundar Bishnoi <sundarlal2929@gmail.com>
2021-09-21 09:25:25 +0530

    commit to master

--- tempPractice.py
+++ tempPractice.py
@@ -1,27 +1,30 @@
-from typing import Counter
+classes = ["MATH 150", "PSYCH 111", "PSYCH 313", "PSYCH 412", "MATH 300", "MATH 404", "MATH 206", "ENG 100", "ENG 103", "ENG 201", "PSYCH 508", "ENG 220", "ENG 125", "ENG 124"]
 
+upper = []
+lower = []
 
-jjj = { }
-print(type(jjj))
+for subject in classes:
+    aclass = subject.split()
+    if aclass[0] == 'MATH':
+        if int(aclass[1]) >= 300 :
+            upper.append(subject)
+        else : 
+            lower.append(subject)
 
-jjj = {'Ramesh' : 12, 'Shyam': 16, 'Hari': 18}
-print(jjj)
+    if aclass[0] == 'ENG' :
+        if int(aclass[1]) >= 200 :
+            upper.append(subject)
+        else : 
+            lower.append(subject)
 
-jjj['Ramesh'] = 55
-print(jjj)
+    if aclass[0] == 'PSYCH' :
+        if int(aclass[1]) >= 400 :
+            upper.append(subject)
+        else : 
+            lower.append(subject)
 
-jjj['Shyam'] = jjj['Shyam'] + 1
-print(jjj)
-
-counts = dict()
-names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
-for name in names :
-    if name not in counts:
-        counts[name] = 1
-    else :
-        counts[name] = counts[name] + 5
-print(counts)
-x = counts.get('Bob',0)
-print(x)
-
-print((0,5,3) < (-1, 6 ,3))
+print(upper)
+print(lower)
+print(len(classes))
+print(len(upper))
+print(len(lower))
