f5099fa9866251523bf8012f1974be9a1cab9dfb
Sundar Bishnoi <sundarlal2929@gmail.com>
2021-09-25 16:18:58 +0530

    OK

--- ex_03_11/ex_03_13_03.py
+++ ex_03_11/ex_03_13_03.py
@@ -19,7 +19,6 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-
 while True:
     address = input('Enter location----')
     if len(address) < 1: break
@@ -40,11 +39,13 @@ while True:
         js = json.loads(data)
     except:
         js = None
-    
+    #print((js['results']))
     if not js or 'status' not in js or js['status'] != 'OK':
         print('=====Failure to retrieve ========')
         print(data)
         continue
+
+    print(js['results'][0]['place_id'])
     lat = js['results'][0]['geometry']['location']['lat']
     lng = js['results'][0]['geometry']['location']['lng']
     print('lat', lat, 'lng', lng)
